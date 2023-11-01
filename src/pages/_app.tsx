ce937f63efbbbb5222c147855d763f72bda540c8
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-01 09:17:37 -0300

    implementado estrutura visual

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Image from "next/image"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
+export default App
