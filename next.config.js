91263d6ac90e9ff172dd7f06a86572382ac121df
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-01 10:58:44 -0300

    implementado integração com o stripe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
